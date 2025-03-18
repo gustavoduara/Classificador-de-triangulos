37288c73dc7b5cd17819128aceb19545a56a88eb
Gustavo Duara <gugaduara@gmail.com>
2025-03-18 15:39:43 -0300

    implementada inicial

--- ClassificacaoDeTrianguloConsoleApp/Program.cs
+++ ClassificacaoDeTrianguloConsoleApp/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.WriteLine("Digite o valor do lado X: ");
+                int LadoX = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Digite o valor do lado Y: ");
+                int LadoY = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Digite o valor do lado Z: ");
+                int LadoZ = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("______________________________________________________");
+                Console.WriteLine("Valores informados: ");
+                Console.WriteLine("Lado X: " + LadoX);
+                Console.WriteLine("Lado Y: " + LadoY);
+                Console.WriteLine("Lado Z: " + LadoZ);
+
+                Console.WriteLine("______________________________________________________");
+                Console.Write("Deseja continuar ? (S / N) : ");
+                string opcaoContinuar = Console.ReadLine() !.ToUpper();
+
+                if (opcaoContinuar != "S")
+                {
+                    break;
+                }
+            }
         }
     }
 }
