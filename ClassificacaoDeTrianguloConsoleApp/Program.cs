fa31f97dd3d32395ab69d7cfd26c4dcc87092586
Gustavo Duara <gugaduara@gmail.com>
2025-03-18 15:28:10 -0300

    Add project files.

---
+++ ClassificacaoDeTrianguloConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassificacaoDeTrianguloConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
